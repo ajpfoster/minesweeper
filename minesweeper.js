c924c0d2bdf549080deb7906f98c7213189ddd32
Ash Foster <ajp.foster@gmail.com>
2019-05-14 13:16:52 +1200

    added game board

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [{row:0, col:0, isMine: true, hidden: true}, {row: 0, col: 1, isMine: false, hidden: true}, {row:0, col:2, isMine: true, hidden: true}, {row: 0, col: 3, isMine: false, hidden: true}, 
+    {row: 1, col: 0, isMine: false, hidden: true}, {row: 1,col:1, isMine: false, hidden: true},{row: 1, col: 2, isMine: false, hidden: true}, {row: 1,col:3, isMine: false, hidden: true},
+    {row: 2, col: 0, isMine: false, hidden: true}, {row: 2,col:1, isMine: false, hidden: true},{row: 2, col: 2, isMine: false, hidden: true}, {row: 2,col:3, isMine: false, hidden: true},
+    {row: 3, col: 0, isMine: false, hidden: true}, {row: 3,col:1, isMine: false, hidden: true},{row: 3, col: 2, isMine: false, hidden: true}, {row: 3,col:3, isMine: false, hidden: true}
+  ]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
@@ -16,14 +22,14 @@ function checkForWin () {
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  lib.displayMessage('You win!')
 }
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
